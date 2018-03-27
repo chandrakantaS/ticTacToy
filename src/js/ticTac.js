0bd6a89e378dd7f20592edace68ff4cd2eff52e3
chandrakanta <ckoo7shatpathy@gmail.com>
2018-03-28 02:27:22 +0530

    impliment level2

--- src/js/ticTac.js
+++ src/js/ticTac.js
@@ -1,14 +1,15 @@
-var markedSquares = [],
+const markedSquares = [],
 textToNumber = {'one': 1,'two': 2,'three': 3,'four': 4,'five': 5,'six': 6,'seven': 7,'eight': 8,'nine': 9},
 numberToText = {1: 'one',2: 'two',3: 'three',4: 'four',5: 'five',6: 'six',7: 'seven',8: 'eight',9: 'nine'},
 winningCombos = [
    [1,2,3], [1,4,7], [2,5,8], [3,6,9], [4,5,6], [1,5,9], [3,5,7], [7,8,9]
-], empty, corners = [1, 3, 7, 9], markedWithX = [], markedWith0 = [];
+], corners = [1, 3, 7, 9], markedWithX = [], markedWith0 = [];
 
+let level;
 function deleteArrEle(arr, ele) {
-   var newArr = arr;
+   let newArr = arr;
    arr = [];
-   newArr.forEach(function(item) {
+   newArr.forEach(item => {
       if (Array.isArray(ele)) {
          if (ele.indexOf(item) === -1) {
             arr.push(item)
@@ -17,35 +18,50 @@ function deleteArrEle(arr, ele) {
          arr.push(item)
       }
    });
-   return arr
+   return arr;
 }
 
-function getPossibleWin(cb) {
+function getPossibleWin() {
    // console.log('markedWith0: ', markedWith0);
+   const promises = [];
+
    remainedEles = []
-   winningCombos.forEach(function(arr) {
-      var matched = [], remainedEle;
-      arr.forEach(function(i) {
-         if (markedWith0.indexOf(i) > -1) {
-            matched.push(i);
-         } else {
-            remainedEle = i;
-         }
-      });
-      if (matched.length === 2 && markedSquares.indexOf(remainedEle) === -1) {
-         // console.log('remainedEle: ', remainedEle)
-         remainedEles.push(remainedEle);
-      }
+   winningCombos.forEach(arr => {
+      promises.push(
+         new Promise(res => {
+            let matched = [], remainedEle;
+            arr.forEach(i => {
+               if (markedWith0.indexOf(i) > -1) {
+                  matched.push(i);
+               } else {
+                  remainedEle = i;
+               }
+            });
+            if (matched.length === 2 && markedSquares.indexOf(remainedEle) === -1) {
+               // console.log('remainedEle: ', remainedEle)
+               res(remainedEle);
+            } else {
+               res('')
+            }
+         })
+      )
+
+
    });
-   cb(remainedEles[0]);
+   return Promise.all(promises).then(ele => {
+      // console.log('ele: ', ele)
+      return ele.filter(e => {
+         return e !== '';
+      });
+   });
 }
 
 function getPossibleDefence(selectedSquare) {
    // console.log('selectedSquare: ', selectedSquare);
-   var possibleWins = [], toDefend = [], ran;
-   winningCombos.forEach(function(arr) {
-      var mayWin = [], leftOut;
-      arr.forEach(function(item) {
+   let possibleWins = [], toDefend = [], ran;
+   winningCombos.forEach(arr => {
+      let mayWin = [], leftOut;
+      arr.forEach(item => {
          if(markedWithX.indexOf(item) > -1) {
             mayWin.push(item);
          }
@@ -59,9 +75,9 @@ function getPossibleDefence(selectedSquare) {
       }
    });
 
-   winningCombos.forEach(function(arr) {
+   winningCombos.forEach(arr => {
       if (arr.indexOf(selectedSquare) > -1) {
-         arr.forEach(function(item) {
+         arr.forEach(item => {
             if (
                toDefend.indexOf(item) == -1 &&
                markedSquares.indexOf(item) == -1
@@ -77,10 +93,10 @@ function getPossibleDefence(selectedSquare) {
    if (possibleWins.length > 0) {
       toDefend = possibleWins;
    } else {
-      winningCombos.forEach(function(arr) {
-         var mayWin = [], leftOut;
+      winningCombos.forEach(arr => {
+         let mayWin = [], leftOut;
          // console.log('markedWith0: ', markedWith0)
-         arr.forEach(function(item) {
+         arr.forEach(item => {
             if(markedWith0.indexOf(item) > -1) {
                mayWin.push(item);
             }
@@ -115,9 +131,9 @@ function getPossibleDefence(selectedSquare) {
 }
 
 function getQueryParams() {
-   var vars = {};
-   var parts = window.location.href.replace(
-      /[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+   let vars = {},
+   parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+   (m,key,value) => {
       vars[key] = value;
    });
    // console.log(vars)
@@ -125,9 +141,12 @@ function getQueryParams() {
 }
 
 function validateForm() {
-
-   var x = document.forms["myForm"]["uname"].value;
-   console.log('x ', x)
+   // console.log('validateFormS')
+   let x = document.forms["myForm"]["uname"].value;
+   x = x.replace(/\s/, '');
+   document.forms["myForm"]["uname"].value = x;
+   // console.log('x ', x)
+   // return false;
    if (x) {
       return true;
    } else {
@@ -142,140 +161,150 @@ function refreshPage() {
 }
 
 function declareWinner(winner) {
-   setTimeout(function(){
+   setTimeout(() => {
       alert(winner + ' won');
-      console.log('some');
+      // console.log('some');
       location.reload();
    }, 500)
-   $('td').unbind("click");
+   //$('td').unbind("click");
 }
 
-$(function(){
-   var params = getQueryParams();
-   // if(!params.uname && !params.level) {
-   //    alert('Please Enter name and level');
-   //    return;
-   // }
-   var h = $('<h2>'+params.uname+'</h2>');
+(function setProfile() {
+   let params = getQueryParams();
+   level = params.level;
+   let h = $('<h2>'+params.uname+'</h2>');
 
    if(params.uname && params.uname.length > 10) {
       h = $('<h2 title='+params.uname+'>'+params.uname.slice(0,7)+'...</h2>');
    }
 
-   var p = $('<p>'+params.level+'</p>')
+   let p = $('<p>'+level+'</p>')
 
    $(document.getElementById('uname')).append(h)
    $(document.getElementById('level')).append(p);
+})();
 
-   $("td").click(function(){
-      // console.log('this" ', this)
-      $(this).css('color', 'red');
-      markedSquares.push(textToNumber[this.id]);
-      markedWithX.push(textToNumber[this.id]);
-      // var div = document.createElement('div');
-      // $(div).text('X')
-      // $(this).append($(div));
-      // $(this).attr('id', 'marked');
-      $(this).text('X');
-      $(this).unbind("click");
-
-      var won
-      winningCombos.forEach(function(arr) {
-         var wonX = [];
-         arr.forEach(function(i) {
-            if (markedWithX.indexOf(i) > -1) {
-               wonX.push(i)
-            }
-
-         });
-         if (wonX.length === 3) {
-            won = wonX;
-            // return;
-         }
-
-      });
-      if (won) {
-         declareWinner('X');
-         // console.log('X won: ', won);
-         // won.forEach(function(i) {
-         //    $(document.getElementById(numberToText[i])).text('X').addClass('winner')
-         // })
-         // $('td').slideDown();
-
-         return;
+function handlePromise (res) {
+   let arr = this;
+   const wonX = [], won0 = [];
+   arr.forEach(i => {
+      if (markedWithX.indexOf(i) > -1) {
+         wonX.push(i)
       }
-
-      // console.log('clicked ', this.id);
-      var squareToMark0, selectedSquare = textToNumber[this.id];
-      // console.log('selectedSquare: ', selectedSquare)
-      if (markedWithX.length === 1) {
-         // squareToMark0 = $('td:empty').attr('id');
-         if (corners.indexOf(selectedSquare) > -1) {
-            squareToMark0 = 'five'
-         } else {
-            squareToMark0 = $('td:empty').attr('id');
-            var ss = $('tbody:empty').attr('id');
-            // console.log('d', ss)
-         }
-         // console.log('squareToMark0' , squareToMark0)
-         var toSelect = $(document.getElementById(squareToMark0));
-
-         // console.log('toSelect', toSelect)
-         toSelect.css('color', 'green')
-         $(toSelect).text('0');
-         markedWith0.push(textToNumber[squareToMark0]);
-         markedSquares.push(textToNumber[squareToMark0])
-         // $(toSelect).attr('id', 'marked');
-         $(toSelect).unbind("click");
-      } else {
-         getPossibleWin(function(ele) {
-            var t;
-            if(ele) {
-               // console.log('sdrfsd ', ele)
-               squareToMark0 = numberToText[ele]
-            } else {
-               // console.log('this.id: ', this.id)
-               squareToMark0 = numberToText[getPossibleDefence(selectedSquare)];
-               var xxx=[];
-               markedWithX.forEach(function(i) {
-                  if(corners.indexOf(i) > -1) {
-                     xxx.push(i)
-                  }
-               });
-
-               if(xxx.length >= 2) {
-                  // console.log('xxx: ', xxx, xxx.length)
-                  // hard logic goes here
-               }
-               // squareToMark0 = ()
-            }
-            // console.log('squareToMark0' , squareToMark0)
-            var toSelect = $(document.getElementById(squareToMark0));
-
-            // console.log('toSelect', toSelect)
-            toSelect.css('color', 'green')
-            $(toSelect).text('0');
-            markedWith0.push(textToNumber[squareToMark0]);
-            markedSquares.push(textToNumber[squareToMark0])
-            // $(toSelect).attr('id', 'marked');
-            winningCombos.forEach(function(arr) {
-               var won0 = [];
-               arr.forEach(function(i) {
-
-                  if (markedWith0.indexOf(i) > -1) {
-                     won0.push(i)
-                  }
-               });
-
-               if (won0.length === 3) {
-                  // console.log('0 won');
-
-                  declareWinner('0');
-                  // return;
-               }
-            });
-            $(toSelect).unbind("click");
-         });
+      if (markedWith0.indexOf(i) > -1) {
+         won0.push(i)
       }
    });
+   if (wonX.length === 3) {
+      res('X');
+   }
+   if(won0.length === 3) {
+      res('0');
+   }
+   else {
+      res('')
+   }
+}
+
+function checkForWin() {
+   const promises = [];
+   winningCombos.forEach(arr => {
+      // console.log('arr: ', arr)
+      promises.push(
+         new Promise(handlePromise.bind(arr))
+      );
+   });
+   return Promise.all(promises).then(s => {
+      return s;
+   })
+}
+
+function getSquareFor0(selectedSquare) {
+
+   return new Promise(async (res) => {
+      let squareFor0;
+      if (markedWithX.length === 1 && parseInt(level) > 1) {
+         // squareToMark0 = $('td:empty').attr('id');
+         if (corners.indexOf(selectedSquare) > -1) {
+            squareFor0 = 'five'
+         } else {
+            let allEmpty = $('td:empty');
+            let randomEmpty = allEmpty[Math.floor(Math.random()*allEmpty.length)]
+            squareFor0 = $(randomEmpty).attr('id');
+            // console.log('d', )
+         }
+         // console.log('squareFor0' , squareFor0)
+      } else {
+         const possibleWin0 = await getPossibleWin();
+         // console.log('possibleWin0: ', possibleWin0)
+         // console.log(typeof level)
+         if (possibleWin0.length > 0 && (parseInt(level) > 1 || Math.random() < 0.5)) {
+            console.log('level: ', level)
+            squareFor0 = numberToText[possibleWin0[0]];
+         } else {
+            squareFor0 = numberToText[getPossibleDefence(selectedSquare)];
+            // console.log('squareFor0: ', squareFor0)
+         }
+      }
+      // console.log('return now: ',squareFor0)
+      res(squareFor0);
+   });
+}
+
+async function markSquareFor0(squareToMark0) {
+   console.log('squareToMark0' , squareToMark0);
+   if (squareToMark0 === undefined) {
+      setTimeout(() => {
+         alert('Match drawn');
+         // console.log('some');
+         location.reload();
+      }, 500)
+      //location.reload();
+   }
+   let toSelect = $(document.getElementById(squareToMark0));
+
+   // console.log('toSelect', toSelect)
+   toSelect.css('color', 'green')
+   $(toSelect).text('0');
+   markedWith0.push(textToNumber[squareToMark0]);
+   markedSquares.push(textToNumber[squareToMark0]);
+
+   if (markedWith0.length > 2) {
+      wins = await checkForWin();
+
+      // console.log(s);
+      if (wins.indexOf('0') > -1) {
+         declareWinner('0');
+      }
+   }
+
+   $(toSelect).unbind("click");
+}
+
+$("td").click(async function(){
+   let squareToMark0, wins,
+   selectedSquare = textToNumber[this.id];
+
+   markedSquares.push(textToNumber[this.id]);
+   markedWithX.push(textToNumber[this.id]);
+   // let div = document.createElement('div');
+   // $(div).text('X')
+   // $(this).append($(div));
+   // $(this).attr('id', 'marked');
+   $(this).text('X');
+   $(this).css('color', 'red');
+   $(this).unbind("click");
+
+   if (markedWithX.length > 2) {
+      wins = await checkForWin();
+
+      // console.log(s);
+      if (wins.indexOf('X') > -1) {
+         declareWinner('X');
+         return;
+      }
+   }
+
+   getSquareFor0(selectedSquare).then(markSquareFor0);
+
 });
